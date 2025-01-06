ce3c569c4e9516fd2ea9bb6228d70cf24f6513d2
Oleg Vrabiy <olegvraby@gmail.com>
2025-01-06 19:42:27 +0200

    update: Label.styles.ts: styled.label.attrs

--- src/components/label/Label.styles.ts
+++ src/components/label/Label.styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-export const StyledLabel = styled.label`
+export const StyledLabel = styled.label.attrs((props) => ({
+  className: props.className,
+}))`
   font-size: 16px;
   color: var(--text-white);
   font-weight: 600;
