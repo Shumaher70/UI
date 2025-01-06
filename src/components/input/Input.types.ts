3f6fb459c6a5d0970a6e12b1d0b9de75f28586a6
Oleg Vrabiy <olegvraby@gmail.com>
2025-01-06 19:44:59 +0200

    update: remove explicit types

--- src/components/input/Input.types.ts
+++ src/components/input/Input.types.ts
@@ -1,47 +1,5 @@
-export interface InputProps {
-  type?:
-    | "text"
-    | "password"
-    | "email"
-    | "number"
-    | "checkbox"
-    | "radio"
-    | "date"
-    | "file"
-    | "range"
-    | "tel"
-    | "url"
-    | "search"
-    | "color"
-    | "datetime-local";
-  placeholder?: string;
-  className?: string;
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+export type StyledInputTypes = Pick<InputProps, "type"> & {
   disabled?: boolean;
-  value?: string;
-  name?: string;
-  id?: string;
-  required?: boolean;
-  autofocus?: boolean;
-  readonly?: boolean;
-  maxlength?: number;
-  minlength?: number;
-  pattern?: string;
-  step?: number;
-  min?: number;
-  max?: number;
-  size?: number;
-  list?: string;
-  autocomplete?: "on" | "off";
-  inputmode?: "text" | "numeric" | "tel" | "email" | "url";
-  form?: string;
-  formaction?: string;
-  formenctype?: string;
-  formmethod?: "get" | "post";
-  formnovalidate?: boolean;
-  formtarget?: "_self" | "_blank" | "_parent" | "_top";
-  ariaLabel?: string; // для доступности
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  onInput?: React.FormEventHandler<HTMLInputElement>;
-  onFocus?: React.FocusEventHandler<HTMLInputElement>;
-  onBlur?: React.FocusEventHandler<HTMLInputElement>;
-}
+};
