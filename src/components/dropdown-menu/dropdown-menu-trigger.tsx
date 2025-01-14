313574e07e3f1ea5720f55e0e04078628b7f21dc
Oleg Vrabiy <olegvraby@gmail.com>
2025-01-14 19:01:16 +0200

    update:dropdown-menu-trigger.tsx:

--- src/components/dropdown-menu/dropdown-menu-trigger.tsx
+++ src/components/dropdown-menu/dropdown-menu-trigger.tsx
@@ -1,13 +1,29 @@
+import { useRef } from "react";
+import { useDropdownMenu } from "./dropdown-menu-provider";
 import { StyledDropdownMenuTrigger } from "./dropdown-menu-trigger.styles";
 import { DropdownMenuTriggerProps } from "./dropdown-menu-trigger.types";
 
 export const DropdownMenuTrigger = ({
-  asChild,
+  asChild = false,
   children,
   ...props
 }: DropdownMenuTriggerProps) => {
+  const triggerRef = useRef<HTMLDivElement | HTMLButtonElement>(null);
+
+  const { setIsOpen, handleTriggerRef } = useDropdownMenu();
+
+  const handleClick = () => {
+    setIsOpen();
+    handleTriggerRef(triggerRef);
+  };
+
   return (
-    <StyledDropdownMenuTrigger as={asChild ? "div" : "button"} {...props}>
+    <StyledDropdownMenuTrigger
+      ref={triggerRef}
+      onClick={handleClick}
+      as={asChild ? "div" : "button"}
+      {...props}
+    >
       {children}
     </StyledDropdownMenuTrigger>
   );
